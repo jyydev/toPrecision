7e0cd5eb6154cc7db3db06e38d0389ea6a3df589
Y <jydev@yahoo.com>
2024-05-26 15:27:52 +0800

    using Number for eliminating exponential notation

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const eToNum = require("from-exponential");
-// import eToNum from "from-exponential";
-const tt = require("greet-codevolution");
-
 function toPrecision(val, prec) {
-  console.log("load to precision");
-  tt("bat");
-  let r = eToNum(Number(val).toPrecision(prec));
-  // let r = "yyy";
+  let r = Number(Number(val).toPrecision(prec));
   return r;
 }
 
 module.exports = toPrecision;
-// export default toPrecision;
