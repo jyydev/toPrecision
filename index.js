949317e2ad6d7fc3ea814f31769bf7dca1e36b95
Y <jydev@yahoo.com>
2024-05-26 15:26:15 +0800

    pre main changes

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const eToNum = require("from-exponential");
+// import eToNum from "from-exponential";
+const tt = require("greet-codevolution");
 
 function toPrecision(val, prec) {
+  console.log("load to precision");
+  tt("bat");
   let r = eToNum(Number(val).toPrecision(prec));
+  // let r = "yyy";
   return r;
 }
 
 module.exports = toPrecision;
+// export default toPrecision;
